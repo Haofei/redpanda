e2de1127040c6d18645eb7042ccefe65a9db1066
Andrew Wong <awong@redpanda.com>
2025-07-21 12:21:48 -0700

    ct/l1: simple_metastore compaction metadata
    Adds an implementation of the compaction-related metastore interface for
    simple_metastore, backed by the new state and state_updates.
    
    As is true generally with the simple_metastore, nothing here is
    persisted at this point, making the simple_metastore ideal for unit
    testing.

--- src/v/cloud_topics/level_one/metastore/simple_metastore.h
+++ src/v/cloud_topics/level_one/metastore/simple_metastore.h
@@ -37,18 +37,11 @@ public:
     get_first_ge(const model::topic_id_partition&, model::timestamp) override;
 
     ss::future<std::expected<void, errc>> compact_objects(
-      const chunked_vector<object_metadata>&,
-      const compaction_map_t&) override {
-        // TODO: implement me!
-        co_return std::unexpected(errc::invalid_request);
-    }
+      const chunked_vector<object_metadata>&, const compaction_map_t&) override;
 
     ss::future<std::expected<compaction_offsets_response, errc>>
     get_compaction_offsets(
-      const model::topic_id_partition&, model::timestamp) override {
-        // TODO: implement me!
-        co_return std::unexpected(errc::invalid_request);
-    }
+      const model::topic_id_partition&, model::timestamp) override;
 
 private:
     state state_;
