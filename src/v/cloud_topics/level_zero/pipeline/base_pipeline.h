f77db667734e632b51cd9fa7520efea664202236
Evgeny Lazin <4lazin@gmail.com>
2025-08-11 12:44:58 -0400

    ct: Add two-step shutdown to pipe
    Pipeline components are supposed to use abort source from the underlying
    pipeline. The idea is that the pipeline is a source of the data for
    components and it also controls when they should stop. This allows us to
    avoid passing abort source into many different places which share the
    pipeline.
    
    The problem is that it doesn't work well with
    'ssx::sharded_service_container'. If the pipeline and all components are
    services the container expects them to stop in certain order and
    naturally, the pipeline is stopped last (because it's created first).
    This makes 'stop' method of the pipeline component to get stuck.
    
    This commit adds a 'shutdown' method. The methods triggers shutdown for
    all components which use pipeline's abort source. The second step is a
    'stop' method that works as before. The 'shutdown' call is optional.
    
    Signed-off-by: Evgeny Lazin <4lazin@gmail.com>

--- src/v/cloud_topics/level_zero/pipeline/base_pipeline.h
+++ src/v/cloud_topics/level_zero/pipeline/base_pipeline.h
@@ -151,9 +151,16 @@ public:
     /// operations.
     basic_retry_chain_node<Clock>& get_root_rtc() noexcept { return _root_rtc; }
 
-    ss::future<> stop() {
+    void shutdown() {
         _as.request_abort_ex(
           std::make_exception_ptr(pipeline_abort_requested()));
+    }
+
+    ss::future<> stop() {
+        if (!_as.abort_requested()) {
+            _as.request_abort_ex(
+              std::make_exception_ptr(pipeline_abort_requested()));
+        }
         auto fut = _gate.close();
         for (auto& f : _filters) {
             f.cancel();
