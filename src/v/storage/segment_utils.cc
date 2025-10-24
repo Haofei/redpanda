316fc1aea3206d9c6d590e42a4620e8ad3587c2c
Willem Kaufmann <willem.kaufmann@redpanda.com>
2025-10-24 16:38:49 -0400

    `storage`: rename `has_transactional_batches` to `may_have...`
    For historical data, we need to consider this value `true` by default-
    consider a multi-segment transaction across segments [1] and [2],
    where [1] contains the `tx_fence`, data batches, and [2] contains the `commit`
    marker. Also assume that for whatever reason, [1] has not been recompacted
    since this `has_transactional_batches` marker was added, but [2] has,
    meaning [1] has `has_transactional_batches=false` and [2] has it as `true`.
    
    When we compact the log, [2] is compacted and potentially has its `commit`
    marker removed while the `tx_fence` in [1] is left unremoved, and the
    `raft_data` batches in [1] are _also_ left with the transactional bit set.
    
    We could get into a situation in which the `raft_data` batches are read,
    and then some `abort` marker in a segment later in the log is mistakenly
    applied, since the `commit` marker is now missing.
    
    Prevent this situation by ensuring that both segments [1] and [2] would
    have `may_have_transactional_batches=true` set even if [1] has not been
    recompacted since the addition of this marker, leading to compaction of [1]
    BEFORE compaction of [2].

--- src/v/storage/segment_utils.cc
+++ src/v/storage/segment_utils.cc
@@ -415,7 +415,7 @@ ss::future<storage::index_state> do_copy_segment_data(
     const bool past_tombstone_delete_horizon
       = internal::is_past_tombstone_delete_horizon(seg, cfg);
     bool may_have_tombstone_records = false;
-    bool has_transaction_batches = false;
+    bool may_have_transaction_batches = false;
 
     auto offset_in_compacted_list =
       [compacted_offsets = std::move(compacted_offsets)](
@@ -434,7 +434,7 @@ ss::future<storage::index_state> do_copy_segment_data(
                           past_tombstone_delete_horizon,
                           &may_have_tombstone_records,
                           &pb,
-                          &has_transaction_batches](
+                          &may_have_transaction_batches](
                            const model::record_batch& b,
                            const model::record& r,
                            bool is_last_record_in_batch) {
@@ -449,7 +449,7 @@ ss::future<storage::index_state> do_copy_segment_data(
           segment_last_offset,
           past_tombstone_delete_horizon,
           may_have_tombstone_records,
-          has_transaction_batches);
+          may_have_transaction_batches);
     };
 
     auto copy_reducer = copy_data_segment_reducer(
@@ -502,8 +502,8 @@ ss::future<storage::index_state> do_copy_segment_data(
     // Set may_have_tombstone_records
     new_index.may_have_tombstone_records = may_have_tombstone_records;
 
-    // Set has_transaction_batches
-    new_index.has_transaction_batches = has_transaction_batches;
+    // Set may_have_transaction_batches
+    new_index.may_have_transaction_batches = may_have_transaction_batches;
 
     if (
       seg->index().may_have_tombstone_records()
@@ -988,9 +988,9 @@ make_concatenated_segment(
 
     // If any of the segments contain a transactional batch, then the new index
     // should reflect that.
-    auto new_has_transaction_batches = std::ranges::any_of(
+    auto new_may_have_transaction_batches = std::ranges::any_of(
       segments,
-      [](const auto& s) { return s->index().has_transaction_batches(); });
+      [](const auto& s) { return s->index().may_have_transaction_batches(); });
 
     segment_index index(
       index_path,
@@ -1002,7 +1002,7 @@ make_concatenated_segment(
       new_clean_compact_timestamp,
       new_may_have_tombstone_records,
       new_self_compact_timestamp,
-      new_has_transaction_batches);
+      new_may_have_transaction_batches);
 
     co_return std::make_tuple(
       ss::make_lw_shared<segment>(
@@ -1406,7 +1406,7 @@ bool is_past_transaction_batch_delete_horizon(
 
 bool has_removable_transaction_batches(
   ss::lw_shared_ptr<segment> seg, const compaction::compaction_config& cfg) {
-    return seg->index().has_transaction_batches()
+    return seg->index().may_have_transaction_batches()
            && is_past_transaction_batch_delete_horizon(seg, cfg);
 }
 
